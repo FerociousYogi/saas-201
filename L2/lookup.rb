254bd3982f674cf5f84eb4dc17fff5f22f42f621
Kairav Shah <kairavshah1702@gmail.com>
2021-01-21 23:56:56 +0530

    completed L2 exercise - DNS Lookup

--- L2/lookup.rb
+++ L2/lookup.rb
@@ -1,3 +1,14 @@
+# Sample Output:
+
+# $ ruby lookup.rb google.com
+# google.com => 172.217.163.46
+
+# $ ruby lookup.rb gmail.com
+# gmail.com => mail.google.com => google.com => 172.217.163.46
+
+# $ ruby lookup.rb gmil.com
+# Error: record not found for gmil.com
+
 def get_command_line_argument
   # ARGV is an array that Ruby defines for us,
   # which contains all the arguments we passed to it
@@ -18,10 +29,28 @@ domain = get_command_line_argument
 # https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
 
-def parse_dns
+dns_raw.filter! { |line| !(line == "" or line == "\n" or line[0] == "#") }
+
+def parse_dns(dns)
+  dns_records = {}
+  dns.map do |line|
+    col = line.split(",").map { |column| column.strip }
+    dns_records[col[1]] = [col[0], col[2]]
+  end
+  dns_records
 end
 
-def resolve
+def resolve(dns_records, lookup_chain, domain)
+  if dns_records.key?(domain)
+    val = dns_records[domain]
+    lookup_chain.push(val[1])
+    if val[0] != "A"
+      resolve(dns_records, lookup_chain, val[1])
+    end
+    lookup_chain
+  else
+    lookup_chain = ["Error: record not found for #{domain}"]
+  end
 end
 
 # To complete the assignment, implement `parse_dns` and `resolve`.
