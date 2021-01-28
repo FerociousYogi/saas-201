37274f3241d0fe08dcb3142e9a9bdf57487ebc84
Kairav Shah <kairavshah1702@gmail.com>
2021-01-28 13:36:21 +0530

    completed L4

--- L4_Active_Record/todo_cli/todo.rb
+++ L4_Active_Record/todo_cli/todo.rb
@@ -1,26 +1,54 @@
-# Expected Output:
-# My Todo-list
-
-# Overdue
-# 24. [X] Submit assignment 2020-02-03
-
-# Due Today
-# 25. [X] Pay rent
-# 28. [ ] Service vehicle
-
-# Due Later
-# 26. [ ] File taxes 2020-02-05
-# 27. [ ] Call Acme Corp. 2020-02-05
-
 require "active_record"
 
 class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
+  def overdue?
+    due_date < Date.today
+  end
+
+  def due_later?
+    due_date > Date.today
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.to_displayable_list(todos)
+    todos.map { |todo| todo.to_displayable_string }
+  end
+
   def self.show_list
+    puts "My Todo-list\n\n"
+
+    puts "Overdue\n"
+    puts to_displayable_list(all.filter { |todo| todo.overdue? })
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts to_displayable_list(all.filter { |todo| todo.due_today? })
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts to_displayable_list(all.filter { |todo| todo.due_later? })
+    puts "\n\n"
   end
 
-  def self.add_task
+  def self.add_task(todo)
+    new_todo = create(todo_text: todo[:todo_text], due_date: Date.today + todo[:due_in_days], completed: false)
+    new_todo.save
+    new_todo
   end
 
-  def self.mark_as_complete!
+  def self.mark_as_complete!(id)
+    todo = all.find(id)
+    todo.completed = true
+    todo.save
+    todo
   end
 end
