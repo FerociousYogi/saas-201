8b4f1e506208e9927dbd0646edca9795abf9192e
Kairav Shah <kairavshah1702@gmail.com>
2021-01-30 15:06:51 +0530

    created scopes

--- L4_Active_Record/todo_cli/todo.rb
+++ L4_Active_Record/todo_cli/todo.rb
@@ -1,13 +1,13 @@
 require "active_record"
 
 class Todo < ActiveRecord::Base
-  def due_today?
-    due_date = Date.today
-  end
+  scope :overdue, -> { where("due_date < ?", Date.today) }
+  scope :due_today, -> { where("due_date = ?", Date.today) }
+  scope :due_later, -> { where("due_date > ?", Date.today) }
 
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
-    display_date = due_today? ? nil : due_date
+    display_date = due_date == Date.today ? nil : due_date
     "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
 
@@ -19,15 +19,15 @@ class Todo < ActiveRecord::Base
     puts "My Todo-list\n\n"
 
     puts "Overdue\n"
-    puts to_displayable_list(all.where("due_date < ?", Date.today).order(:id))
+    puts to_displayable_list(all.overdue.order(:id))
     puts "\n\n"
 
     puts "Due Today\n"
-    puts to_displayable_list(all.where("due_date = ?", Date.today).order(:id))
+    puts to_displayable_list(all.due_today.order(:id))
     puts "\n\n"
 
     puts "Due Later\n"
-    puts to_displayable_list(all.where("due_date > ?", Date.today).order(:id))
+    puts to_displayable_list(all.due_later.order(:id))
     puts "\n\n"
   end
 
